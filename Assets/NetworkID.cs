f27a25c00a8f591dcdf7f485a5604f218f69e931
Tariv093 <tariv093@gmail.com>
2020-10-08 12:21:21 -0400

    assignment 1

---
+++ Assets/NetworkID.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkID : MonoBehaviour
+{
+    [SerializeField]
+    public string id;
+    public Color color = new Color(0, 0, 0);
+    public Vector3 pos = new Vector3(0, 0, 0);
+    public bool myCube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.color = color;
+        //if your cube, do not update pos
+        if (myCube == false)
+        {
+            transform.position = pos;
+        }
+            //if not your cube, update pos
+    }
+
+  public bool getBool()
+    {
+        return myCube;
+    }
+
+  public void setBool(bool b)
+    {
+        myCube = b;
+    }
+
+}
--- Assets/NetworkMan.cs
+++ Assets/NetworkMan.cs
@@ -5,39 +5,62 @@ using System;
 using System.Text;
 using System.Net.Sockets;
 using System.Net;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Security.Cryptography;
 
 public class NetworkMan : MonoBehaviour
 {
+    public GameObject prefab;
     public UdpClient udp;
+    public Dictionary<string, GameObject> existingPlayers = new Dictionary<string, GameObject>();
+    public List<string> playerIDs;
+    public List<string> leavingPlayers;
+    private string myClientID;
     // Start is called before the first frame update
+    [Serializable]
+    public class updateMessage
+    {
+        public string cmd;
+
+        public float X;
+        public float Y;
+        public float Z;
+    }
     void Start()
     {
         udp = new UdpClient();
-        
-        udp.Connect("PUT_IP_ADDRESS_HERE",12345);
-
-        Byte[] sendBytes = Encoding.ASCII.GetBytes("connect");
-      
-        udp.Send(sendBytes, sendBytes.Length);
-
+      //  3.131.90.180
+        udp.Connect("18.217.121.107", 12345);
+           updateMessage uMsg = new updateMessage();
+        uMsg.cmd = "connect";
+        JsonUtility.ToJson(uMsg);
+        Byte[] sendBytes = Encoding.ASCII.GetBytes(JsonUtility.ToJson(uMsg));
+        udp.Send(sendBytes, sendBytes.Length); 
         udp.BeginReceive(new AsyncCallback(OnReceived), udp);
 
         InvokeRepeating("HeartBeat", 1, 1);
+        InvokeRepeating("UpdatePosition", 1,1/30);
     }
 
     void OnDestroy(){
         udp.Dispose();
+        
     }
 
 
     public enum commands{
         NEW_CLIENT,
-        UPDATE
+        UPDATE,
+        DISCONNECT,
+        CONFIRMID
+
     };
     
     [Serializable]
     public class Message{
         public commands cmd;
+        public string id;
     }
     
     [Serializable]
@@ -49,7 +72,8 @@ public class NetworkMan : MonoBehaviour
             public float B;
         }
         public string id;
-        public receivedColor color;        
+        public receivedColor color;
+        public float posX, posY, posZ;
     }
 
     [Serializable]
@@ -76,49 +100,152 @@ public class NetworkMan : MonoBehaviour
         
         // do what you'd like with `message` here:
         string returnData = Encoding.ASCII.GetString(message);
-        Debug.Log("Got this: " + returnData);
+        UnityEngine.Debug.Log("Got this: " + returnData);
         
         latestMessage = JsonUtility.FromJson<Message>(returnData);
         try{
             switch(latestMessage.cmd){
                 case commands.NEW_CLIENT:
+
+                    playerIDs.Add(latestMessage.id);
+                    UnityEngine.Debug.Log(latestMessage.id);
                     break;
                 case commands.UPDATE:
                     lastestGameState = JsonUtility.FromJson<GameState>(returnData);
+                    //.Log(lastestGameState.players[0].color.R);
+                    break;
+                case commands.DISCONNECT:
+                    leavingPlayers.Add(latestMessage.id);
+                    break;
+                case commands.CONFIRMID:
+                    myClientID = latestMessage.id;
                     break;
                 default:
-                    Debug.Log("Error");
+                    UnityEngine.Debug.Log("Error");
                     break;
             }
         }
         catch (Exception e){
-            Debug.Log(e.ToString());
+           UnityEngine.Debug.Log(e.ToString());
         }
         
         // schedule the next receive operation once reading is done:
         socket.BeginReceive(new AsyncCallback(OnReceived), socket);
     }
 
-    void SpawnPlayers(){
+    void SpawnPlayers()
+    {
+       // Vector3 pos = new Vector3(UnityEngine.Random.Range(0, 10), UnityEngine.Random.Range(0, 10),0);
 
+        foreach(string s in playerIDs)
+        {
+          
+            
+            if(s == null)
+            {
+
+            }
+            else if(!existingPlayers.ContainsKey(s))
+            {
+                existingPlayers.Add(s, Instantiate(prefab, new Vector3(0,0,0), Quaternion.identity));
+                existingPlayers[s].GetComponent<NetworkID>().id = s;
+            }
+            
+        }
+        playerIDs.Clear();
+   
     }
 
-    void UpdatePlayers(){
+    void UpdatePlayers() {
+
+     //  existingPlayers
+     for(int i = 0; i < lastestGameState.players.Length; i++)
+        {
+            if (existingPlayers.ContainsKey(lastestGameState.players[i].id))
+            {
+           
+                existingPlayers[lastestGameState.players[i].id].GetComponent<NetworkID>().color = new Color(lastestGameState.players[i].color.R, lastestGameState.players[i].color.G, lastestGameState.players[i].color.B);
+                existingPlayers[lastestGameState.players[i].id].GetComponent<NetworkID>().pos = new Vector3(lastestGameState.players[i].posX, lastestGameState.players[i].posY, lastestGameState.players[i].posZ);
+
+            }
+            else
+            {
+                playerIDs.Add(lastestGameState.players[i].id);
+            }
+        }
 
     }
 
     void DestroyPlayers(){
+       // OnDestroy();
 
+        foreach(string s in leavingPlayers)
+        {
+            if(s == null)
+            {
+
+            }
+            else if (existingPlayers.ContainsKey(s))
+            {
+                Destroy(existingPlayers[s]);
+                existingPlayers.Remove(s);
+            }
+        }
+        leavingPlayers.Clear();
     }
     
     void HeartBeat(){
-        Byte[] sendBytes = Encoding.ASCII.GetBytes("heartbeat");
+        updateMessage uMsg = new updateMessage();
+        uMsg.cmd = "heartbeat";
+        JsonUtility.ToJson(uMsg);
+        Byte[] sendBytes = Encoding.ASCII.GetBytes(JsonUtility.ToJson(uMsg));
         udp.Send(sendBytes, sendBytes.Length);
     }
-
+    void UpdatePosition()
+    {
+        if (myClientID == null)
+        { return; }
+        if (existingPlayers.ContainsKey(myClientID))
+        {
+            updateMessage uMsg = new updateMessage();
+            uMsg.cmd = "updateposition";
+            uMsg.Y = existingPlayers[myClientID].transform.position.y;
+            uMsg.X = existingPlayers[myClientID].transform.position.x;
+            uMsg.Z = existingPlayers[myClientID].transform.position.z;
+            JsonUtility.ToJson(uMsg);
+            Byte[] sendBytes = Encoding.ASCII.GetBytes(JsonUtility.ToJson(uMsg));
+            udp.Send(sendBytes, sendBytes.Length);
+            existingPlayers[myClientID].GetComponent<NetworkID>().setBool(true);
+        }
+        else
+        {
+            existingPlayers[myClientID].GetComponent<NetworkID>().setBool(false);
+        }
+    }
     void Update(){
         SpawnPlayers();
         UpdatePlayers();
         DestroyPlayers();
+
+
+      if(Input.GetKey("w"))
+        {
+            //use jsons to translate your position to serverside, which will then translate the player in a direction
+            //translate
+            existingPlayers[myClientID].transform.Translate(new Vector3(0, 1*Time.deltaTime, 0));
+        }
+      if(Input.GetKey("s"))
+        {
+
+        }
+        //if (Input.GetKey("A"))
+        //    {
+        //    Debug.Log("moving left");
+        //}
+    }
+
+     void OnApplicationQuit()
+    {
+        OnDestroy();
     }
 }
